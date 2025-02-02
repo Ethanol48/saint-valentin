cbbad9c78a1551c2a3bbb17d29f83b1a3ad2bab4
Ethan Rouimi <contact@ethan-rouimi.com>
2025-02-02 13:56:20 +0100

    erase everything that is not needed

--- src/lib/supabase.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://YOUR_PROJECT_URL.supabase.co';
-const supabaseKey = 'YOUR_SUPABASE_KEY';
-export const supabase = createClient(supabaseUrl, supabaseKey);
